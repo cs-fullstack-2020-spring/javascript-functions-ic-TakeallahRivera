131c56bb92e47a26ec59c9f43a361f65a595f5e2
Takeallah <takeallahsrivera@icloud.com>
2020-02-21 11:29:42 -0600

    InClassPush2

--- InClass.js
+++ InClass.js
@@ -1,3 +1,61 @@
-let inClass = "inClassAssignment";
+/*### Exercise 1:
+- Ask the user to enter two numbers
+- Create a function that takes two numbers and retruns the sum
+- Print the sum of the numbers the user entered 
+to the console using the function you created
 
-console.log(inClass);
+```
+Example Output
+
+if the user enters 10 and 15 
+
+The sum of 10 and 15 is 25
+
+*/
+
+//Exercise One Solution//
+
+/*function addUserNumbers(num1, num2){
+    return (num1 + num2);
+}
+let userInputNumber1 = parseInt(prompt("Enter a number"));
+let userInputNumber2 = parseInt(prompt("Enter another number"));
+let sum = addUserNumbers(userInputNumber1,userInputNumber2) 
+console.log(`The sum of ${userInputNumber1} and ${userInputNumber2} is ${sum}`); 
+function addtUserNumbers(num1, num2){
+    return (num1 - num2);
+}
+*/
+
+/*### Exercise 2:
+- Create a function that takes any array and prints 
+each elememt in the array
+```
+Example Output
+
+if you pass in an array with the value 
+["Autumn", "Kevin", "Kenn"]
+
+Autumn
+Kevin
+Kenn
+```
+*/
+
+//Exercise 2 Solution//
+
+/*let nameArray =['Takeallah', 'Roxanne', 'Devere', 'Jazz'];
+function printArrayName(nameArray);
+
+console.log (printArrayName);
+
+*/
+
+/*### Exercise 3:
+- Create a function that asks the user to enter a name 
+until they enter q or Q
+- if they enter "Kevin" call a function that alerts "Cool Dude"
+*/
+
+//Exercise 3 Solution//
+
